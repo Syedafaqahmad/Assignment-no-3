f24eac678097c599ffd4db944eb4d4578b6fa720
Syedafaqahmad <82577242+Syedafaqahmad@users.noreply.github.com>
2024-02-05 08:19:11 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// Question=1
+
+// var city = prompt ("Enter your City Name") 
+// if (city === "Karachi") {
+//     alert("Welcome to city of Light")
+// }
+
+// Question=2 
+
+var Gender = prompt("Enter Your Gender")
+if (Gender === "Male") {
+    alert("Good Morning Sir")
+}
+if(Gender === "Female") {
+    alert("Good Morning Ma'am")
+}
